5835baed4c70e1e05f388d7b50159f2a0c4cb5c4
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-07-16 20:05:37 -0700

    Change bcrypt-ruby to bcrypt and fix typo in condition association

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,6 +1,6 @@
 class Condition < ApplicationRecord
   has_many :attack_options_conditions
-  has_many :attack_options, thtrough: :attack_options_conditions
+  has_many :attack_options, through: :attack_options_conditions
   has_many :damage_types, through: :attack_options
   has_many :weapons, through: :attack_options
   has_many :weapon_classes, through: :weapons
