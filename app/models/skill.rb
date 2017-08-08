1a9d3254d5991aaaf4f78dc4924b1128ffb09ce3
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-08-08 16:16:04 -0700

    Add #cost_at_rank to skills and test it. Also make default ranks = 1

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,4 +4,13 @@ class Skill < ApplicationRecord
 
   has_many :obtained_skills
   has_many :characters, through: :obtained_skills
+
+  def cost_at_rank(rank)
+    skill_cost = SkillCost.find_by(skill_id: self.id, rank: rank)
+    if skill_cost
+      skill_cost.cost
+    else
+      skill_cost
+    end
+  end
 end
