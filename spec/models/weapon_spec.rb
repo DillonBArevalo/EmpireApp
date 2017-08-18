fd297df89637fce5189853ff55aa60d31d7b7047
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-08-18 12:22:14 -0700

    Add description to armors in schema and refactor tests for is shield in weapon spec

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -23,20 +23,21 @@ RSpec.describe Weapon, type: :model do
   end
 
   describe '#is_shield?' do
+    let(:core_user) {User.create(username: 'Empire: core', email: 'dillonbarevalo@gmail.com', name: 'Dillon and Jack', password: 'donothackme', password_confirmation: 'donothackme')}
+    let(:light_shield) {WeaponType.create!(name: 'Light shield')}
+    let(:shields) {WeaponClass.create!(name: 'Shields', description: "U")}
+    let(:shield) {Weapon.new(user_id: core_user.id, weapon_type_id: light_shield.id, name: 'Light Shield', description: '', defense_die_number: 1, defense_die_size: 10, flat_defense_bonus: 4, defense_energy_modifier: 2, extra_block_cost: 30, hands_used: 1)}
+
+    let(:class1) {WeaponClass.create!(name: 'Power Weapons', description: "Th")}
+    let(:axe) {WeaponType.create!(name: 'Axe')}
+    let(:b_axe) {Weapon.new(user_id: core_user.id, weapon_type_id: axe.id, name: 'Battle Axe', description: '', defense_die_number: 1, defense_die_size: 10, flat_defense_bonus: 5, defense_energy_modifier: 1, extra_block_cost: 30, extra_attack_cost: 30, hands_used: 2, dodge_energy_mod_penalty: 0.5)}
+
     it 'returns true when the weapon is a shield' do
-      core_user = User.create(username: 'Empire: core', email: 'dillonbarevalo@gmail.com', name: 'Dillon and Jack', password: 'donothackme', password_confirmation: 'donothackme')
-      light_shield = WeaponType.create!(name: 'Light shield')
-      shield = Weapon.new(user_id: core_user.id, weapon_type_id: light_shield.id, name: 'Light Shield', description: '', defense_die_number: 1, defense_die_size: 10, flat_defense_bonus: 4, defense_energy_modifier: 2, extra_block_cost: 30, hands_used: 1)
-      shields = WeaponClass.create!(name: 'Shields', description: "U")
       shield.weapon_classes << shields
       expect(shield.is_shield?).to be true
     end
 
     it 'returns false otherwise' do
-      core_user = User.create(username: 'Empire: core', email: 'dillonbarevalo@gmail.com', name: 'Dillon and Jack', password: 'donothackme', password_confirmation: 'donothackme')
-      axe = WeaponType.create!(name: 'Axe')
-      b_axe = Weapon.new(user_id: core_user.id, weapon_type_id: axe.id, name: 'Battle Axe', description: '', defense_die_number: 1, defense_die_size: 10, flat_defense_bonus: 5, defense_energy_modifier: 1, extra_block_cost: 30, extra_attack_cost: 30, hands_used: 2, dodge_energy_mod_penalty: 0.5)
-      class1 = WeaponClass.create!(name: 'Power Weapons', description: "Th")
       b_axe.weapon_classes << class1
       expect(b_axe.is_shield?).to be false
     end
