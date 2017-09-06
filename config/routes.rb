57a6d064900456ec29947e0fdc40906ccb798d3b
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-09-05 22:16:45 -0700

    Add first draft of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :show, :create]
 
+  resources :armors #, except: [:edit, :update, :destroy] # full
+  resources :weapons # full
+  # resources :armor_types
+  resources :characters do #, except: [:edit, :update, :destroy] # full
+    resources :inventories, only: [:show]
+  end
+
+  resources :damage_resistances, only: [:index] # describe dr?
+  resources :damage_types, only: [:index] # describe/display
+
+  resources :character_classes, only: [:index, :show]
+  resources :conditions, only: [:index, :show]
+  resources :skills, only: [:index, :show]
+  resources :weapon_classes, only: [:index, :show]
+  resources :weapon_types, only: [:index, :show]
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
