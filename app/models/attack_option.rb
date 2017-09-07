df1ffd19551b4e7b9e9805cb5315c4d06193fab1
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-09-07 01:23:24 -0700

    Fix validations on weapon and attack option so seeds can run (and so they are right) and add default character

--- app/models/attack_option.rb
+++ app/models/attack_option.rb
@@ -6,5 +6,5 @@ class AttackOption < ApplicationRecord
   has_many :attack_options_conditions
   has_many :conditions, through: :attack_options_conditions
 
-  validates :name, :weapon, :damage_type, :energy_modifier, :die_number, :die_size, :attack_bonus, :damage_dice, :damage_die_size, :strength_damage_bonus, :dexterity_damage_bonus, :flat_damage_bonus, presence: true
+  validates :name, :weapon, :damage_type, :energy_modifier, :die_number, :die_size, :attack_bonus, :damage_dice, :damage_die_size, :flat_damage_bonus, presence: true
 end
