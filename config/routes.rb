4f9e1da539fb563aae376bb6c4461160af04cce6
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-09-23 21:27:20 -0700

    Add users index and slightly edit weapon classes show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :show, :create]
+  resources :users, only: [:index, :new, :show, :create]
 
   resources :armors # full
   resources :weapons # full
